3e6f76b98dc041458c5b5c1581df162b4e680576
Amilton Junior <amilton_junior@msn.com>
2018-07-18 16:55:36 -0300

    Adicionados os ponteiros para os elementos e os listeners dos objetos da EditPersonActivity

--- app/src/main/java/com/amiltonsoft/amiltonjunior/pessoalandroid/EditPersonActivity.java
+++ app/src/main/java/com/amiltonsoft/amiltonjunior/pessoalandroid/EditPersonActivity.java
@@ -3,7 +3,10 @@ package com.amiltonsoft.amiltonjunior.pessoalandroid;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class EditPersonActivity extends AppCompatActivity {
@@ -15,14 +18,52 @@ public class EditPersonActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Manipulação do spinner
-        Spinner s = (Spinner) findViewById(R.id.spinnerSex);
+        // Cria os ponteiros para os elementos da activity
+        EditText name   = (EditText) findViewById(R.id.editTextName);
+        EditText age    = (EditText) findViewById(R.id.editTextAge);
+        Spinner sex     = (Spinner) findViewById(R.id.spinnerSex);
+        Button save     = (Button) findViewById(R.id.btnSave);
+        Button cancel   = (Button) findViewById(R.id.btnCancel);
+        Button remove   = (Button) findViewById(R.id.btnRemove);
 
         // Adiciona as opções ao spinner
-        String[] arraySpinner = new String[] { "M", "F" };
+        String[] arraySpinner = new String[] { "M", "F" }; // Tipos de sexo
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arraySpinner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        s.setAdapter(adapter);
+        sex.setAdapter(adapter);
+
+        // Listener do botão "Salvar"
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                System.out.println("Botão Salvar clicado!");
+
+            }
+        });
+
+        // Listener do botão "Cancelar"
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //System.out.println("Botão Cancelar clicado!");
+
+                // Encerra a activity
+                finish();
+
+            }
+        });
+
+        // Listener do botão "Excluir"
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                System.out.println("Botão Excluir clicado!");
+
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
