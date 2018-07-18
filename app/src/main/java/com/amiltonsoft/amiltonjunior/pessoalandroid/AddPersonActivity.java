f608d98b800c48c16304cc7435d76098fef98165
Amilton Junior <amilton_junior@msn.com>
2018-07-18 17:24:00 -0300

    Adicionados os listeners dos botões da AddPersonActivity e preenchimento do spinner

--- app/src/main/java/com/amiltonsoft/amiltonjunior/pessoalandroid/AddPersonActivity.java
+++ app/src/main/java/com/amiltonsoft/amiltonjunior/pessoalandroid/AddPersonActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -25,6 +26,35 @@ public class AddPersonActivity extends AppCompatActivity {
         Spinner sex     = (Spinner) findViewById(R.id.spinnerSex);
         Button save     = (Button) findViewById(R.id.btnSave);
         Button cancel   = (Button) findViewById(R.id.btnCancel);
+
+        // Adiciona as opções ao spinner
+        String[] arraySpinner = new String[] { "M", "F" }; // Tipos de sexo
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sex.setAdapter(adapter);
+
+        // Listener do botão "Salvar"
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                System.out.println("Botão Salvar clicado!");
+
+            }
+        });
+
+        // Listener do botão "Cancelar"
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //System.out.println("Botão Cancelar clicado!");
+
+                // Encerra a activity
+                finish();
+
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
