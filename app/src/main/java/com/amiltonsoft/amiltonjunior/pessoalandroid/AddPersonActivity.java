62a18ce921b6a477687437909385d3b2013a50f1
Amilton Junior <amilton_junior@msn.com>
2018-07-18 17:19:12 -0300

    Corrigido alinhamento dos botões, removido o botão "excluir" e adicionados os ponteiros na AddPersonActivity

--- app/src/main/java/com/amiltonsoft/amiltonjunior/pessoalandroid/AddPersonActivity.java
+++ app/src/main/java/com/amiltonsoft/amiltonjunior/pessoalandroid/AddPersonActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddPersonActivity extends AppCompatActivity {
 
@@ -16,14 +19,13 @@ public class AddPersonActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Cria os ponteiros para os elementos da activity
+        EditText name   = (EditText) findViewById(R.id.editTextName);
+        EditText age    = (EditText) findViewById(R.id.editTextAge);
+        Spinner sex     = (Spinner) findViewById(R.id.spinnerSex);
+        Button save     = (Button) findViewById(R.id.btnSave);
+        Button cancel   = (Button) findViewById(R.id.btnCancel);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
