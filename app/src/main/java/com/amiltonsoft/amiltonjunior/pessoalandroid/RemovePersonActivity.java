b644f490aae4aaafe41d26387aa9a15ed54ceddb
Amilton Junior <amilton_junior@msn.com>
2018-07-18 16:57:09 -0300

    Adicionada a RemovePersonActivity

---
+++ app/src/main/java/com/amiltonsoft/amiltonjunior/pessoalandroid/RemovePersonActivity.java
@@ -0,0 +1,24 @@
+package com.amiltonsoft.amiltonjunior.pessoalandroid;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class RemovePersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remove_person);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
